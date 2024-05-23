b24a5acfa1a5f8ea09f39a2863ec624931ec8d7c
Roman Boiko <Roman.Boiko@tui.co.uk>
2024-05-23 16:54:05 +0300

    automated Accessibility Testing Tools

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import axe from 'react-axe'
 import './index.css'
+
+if (process.env.NODE_ENV !== 'production') {
+  axe(React, ReactDOM, 1000);
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
